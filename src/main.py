9c5895dbb41827c234c13a71763c7fbe4b59dc14
Jeremy Croiset <jeremy.croiset@epita.fr>
2022-07-11 18:49:07 +0200

    chore: add popups

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 import argparse
+import ctypes
 import logging
 
 import dofus_patcher
+
+
+def popup(text: str, style: int = 0):
+    return ctypes.windll.user32.MessageBoxW(0, text, "Dofus Helper", style)
 
 
 def main(debug=False):
@@ -12,15 +17,27 @@ def main(debug=False):
     try:
         patcher = dofus_patcher.DofusPatcher()
     except Exception as e:
-        exit(e)
+        if not debug:
+            popup(
+                "Unable to find the game process.\n"
+                "Make sure the game is open and on the authentication screen."
+            )
+        else:
+            print(e)
+        exit()
 
     if not patcher.apply():
-        exit(
-            "Failed to apply patch. Aborting.\n"
-            "Make sure the game is on the Authentication screen."
-        )
+        if not debug:
+            popup(
+                "Failed to apply patch.\n"
+                "Make sure the game is on the authentication screen."
+            )
+        exit()
 
-    print("Success!")
+    if debug:
+        print("Success!")
+    else:
+        popup("Successfully patched!")
 
 
 if __name__ == "__main__":
